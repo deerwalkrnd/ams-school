ea9d69cc59c971d20b9594abe5eb1f06ae299b05
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-12-05 17:37:32 +0545

    feat (form/section): table added for section section

--- resources/views/layouts/admin/formTabs.blade.php
+++ resources/views/layouts/admin/formTabs.blade.php
@@ -2,11 +2,11 @@
     <div class="wrapper-nav">
         <nav class="nav nav-tabs list mt-2 d-flex justify-content-around " id="myTab" role="tablist">
         <!-- <nav class="nav nav-tabs list mt-2" id="myTab" role="tablist"> -->
-            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab"
+            <a class="nav-item nav-link nav_item below_header_btn" href="{{route('user.index')}}" role="tab"
                 aria-controls="public" aria-expanded="true">Users</a>
             <!-- <a class="nav-item nav-link nav_item below_header_btn active" data-toggle="tab" href="#tab1"
                             role="tab" aria-controls="public" aria-expanded="true">Tab1</a> -->
-            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Section</a>
+            <a class="nav-item nav-link nav_item below_header_btn" href="{{route('section.index')}}" role="tab">Section</a>
             <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Grade</a>
             {{-- <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Students</a> --}}
             {{-- <a class="nav-item nav-link nav_item below_header_btn" href="{{route('subject.list')}}" role="tab">Subject</a> --}}
