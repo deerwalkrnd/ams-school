d8fce1f9b1097400f34ab63c4d2343b083715e65
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-12-05 15:23:03 +0545

    feat (forms/users): added datatable on users dashboard

---
+++ resources/views/layouts/admin/formTabs.blade.php
@@ -0,0 +1,22 @@
+<div class="w-100 pt-3">
+    <div class="wrapper-nav">
+        <nav class="nav nav-tabs list mt-2 d-flex justify-content-around " id="myTab" role="tablist">
+        <!-- <nav class="nav nav-tabs list mt-2" id="myTab" role="tablist"> -->
+            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab"
+                aria-controls="public" aria-expanded="true">Users</a>
+            <!-- <a class="nav-item nav-link nav_item below_header_btn active" data-toggle="tab" href="#tab1"
+                            role="tab" aria-controls="public" aria-expanded="true">Tab1</a> -->
+            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Section</a>
+            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Grade</a>
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Students</a> --}}
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="{{route('subject.list')}}" role="tab">Subject</a> --}}
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="{{route('group.list')}}" role="tab">Group</a> --}}
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="{{route('subject_group_assign.list')}}" role="tab">Section Subject</a> --}}
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Section Student</a> --}}
+            {{-- <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Teacher Section Subject</a> --}}
+            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Holiday</a>
+            <a class="nav-item nav-link nav_item below_header_btn" href="" role="tab">Feedback Email</a>
+
+        </nav>
+    </div>
+</div>
