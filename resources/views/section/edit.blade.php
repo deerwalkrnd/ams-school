0704e78b7ee944bfa83dc39b6483a968ab819f1f
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-12-11 12:50:48 +0545

    (feat edit layout): updated edit page layout

--- resources/views/section/edit.blade.php
+++ resources/views/section/edit.blade.php
@@ -1,59 +1,43 @@
- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit </title>
-</head>
-
-<body>
-    @if ($errors->all())
-        @foreach ($errors->all() as $error)
-            <div class="alert">
-                {{ $error }}
-            </div>
-        @endforeach
-    @endif
-    <form action="{{route('section.update',['id'=>$sections->id])}}" method="post">
+<x-edit-layout>
+    <form action="{{ route('section.update', ['id' => $sections->id]) }}" method="post">
         @csrf
         @method('PUT')
         <div>
-            <label for="name"> Section Name</label>
-            <input type="text" name="name" value="{{$sections->name}}" required>
+            <label for="name">Section Name<span class="star">*</span></label>
+            <div class="input_container">
+                <input type="text" name="name" value="{{ $sections->name }}" required>
+            </div>
         </div>
         <div>
             <div class="form-group">
-                <label for="Choose Section Type">Choose Section Type</label>
-                <select name="type" class="form-control">
-                    <option value="optional">Optional</option>
-                    <option value="compulsory">Compulsory</option>
-                </select>
+                <label for="Choose Section Type">Choose Section Type<span class="star">*</span></label>
+                <div class="input_container">
+                    <select name="type" class="form-control">
+                        <option value="optional">Optional</option>
+                        <option value="compulsory">Compulsory</option>
+                    </select>
+                </div>
             </div>
         </div>
-        <label for="">Grade</label>
-        <select name="grade_id">
-            @foreach ($grades as $grade)
-                <option value="{{ $grade->id }}">{{ $grade->name }}</option>
-            @endforeach
-        </select>
-        <label for="">Teacher</label>
-        <select name="user_id">
-            @foreach ($users as $user)
-                <option value="{{ $user->id }}">{{ $user->name }}</option>
-            @endforeach
-        </select>
+        <label for="">Grade<span class="star">*</span></label>
+        <div class="input_container">
+            <select name="grade_id">
+                @foreach ($grades as $grade)
+                    <option value="{{ $grade->id }}">{{ $grade->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <label for="">Teacher<span class="star">*</span></label>
+        <div class="input_container">
+            <select name="user_id">
+                @foreach ($users as $user)
+                    <option value="{{ $user->id }}">{{ $user->name }}</option>
+                @endforeach
+            </select>
+        </div>
         <br>
-        <button class="btn btn-primary" type="submit">Add</button>
+        <button class="btn btn-primary" type="submit">Update</button>
 
     </form>
 
-</body>
-<script>
-    setTimeout(() => {
-        document.querySelector('.alert').style.display = 'none';
-    }, 2000);
-</script>
-
-</html>
+</x-edit-layout>
