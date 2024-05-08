16ba6551ffc9336b6543ab7f1c707ae41dfab4e2
Suyash Adhikari <adhikarisuyash23@gmail.com>
2024-05-08 15:44:48 +0545

    updated dropped student logic

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -73,6 +73,7 @@
         <thead>
             <tr class="table_title">
                 <th>Student's Name</th>
+                @if($attendanceDates)
                 @forelse ($attendanceDates as $date)
                     <th colspan="2" class="text-center border-end">
                         {{ $date }}
@@ -84,17 +85,19 @@
                         </h5>
                     </td>
                 @endforelse
+                @endif
             </tr>
         </thead>
         <tbody>
             @foreach ($students as $student)
-                <tr >
+            @if($student->status=='active')
+                <tr>
                     <td class="border-end">{{ $student->name }}</td>
                     @forelse ($student->getAttendances($startDate??null, $endDate??null) as $dateOfAttendance)
                         <td class="border-end text-center ">
                             @if ($dateOfAttendance['present'] > 0)
                                 @for ($i = 1; $i <= $dateOfAttendance['present']; $i++)
-                                    <span class="attendanceSymbol presentSymbol">P</span>
+                                    <span class="attendanceSymbol presentSymbol ">P</span>
                                 @endfor
                             @endif
                             @if ($dateOfAttendance['absent'] > 0)
@@ -108,6 +111,7 @@
                         <td class="text-center border-end"> Attendance has not been taken. </td>
                     @endforelse
                 </tr>
+                @endif
             @endforeach
         </tbody>
         <tfoot>
