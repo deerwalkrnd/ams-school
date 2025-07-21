f33e83edf2a8e5b7b16fa1d4fa934947b0a2e1c5
Supritphuyal <supritphuyal123@gmail.com>
2025-07-21 17:50:07 +0545

    Added migrations

---
+++ database/migrations/2023_10_12_041330_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('roll_no')->unique();
+            $table->string('name');
+            $table->string('email');
+            $table->enum('status', ['active', 'dropped_out']);
+            $table->unsignedBigInteger('section_id');
+            $table->timestamps();
+            $table->foreign('section_id')->references('id')->on('sections')->cascadeOnUpdate()->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
